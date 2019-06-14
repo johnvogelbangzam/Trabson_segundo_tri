5a4d79e2d9c90bad5dba434fd60c119d86595167
João Paulo Ludvig Vogel <john.vogel1317@gmail.com>
2019-06-14 09:32:21 -0300

    Manda la
    Tela de login

--- Index.php
+++ Index.php
@@ -1,6 +1,25 @@
+<!DOCTYPE html>
 <html lang="en">
-    <form method="POST">
-        <input type="text" placeholder="Login">
-        <input type="password" name="senha" id="senha" placeholder="Senha">
-    </form>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Layout com Grid e Flex</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+</head>
+<body>
+    <header>
+        <h1>Randomizador de Lugares</h1>
+    </header>
+    <section>
+        <form class="login" method="POST" action="Processa.php">
+            <div class="login"><input type="text" placeholder="Login"></div>
+            <div class="login"><input type="password" name="senha" id="senha" placeholder="Senha"></div>
+            <div class="login"><button type="button">Enviar</button></div>
+        </form>
+    </section>
+    <footer>
+        <h5>Feito por: Gabriel Ledur, Giovane Müller e João Vogel</h5>
+    </footer>
+    </body>
 </html>
